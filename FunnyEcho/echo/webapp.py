3f54f77e4d7fb69d21c4e64c7c31f06f032c6a78
fcorno <fulvio.corno@polito.it>
2015-04-23 15:54:35 +0200

    First version (HTML and CSS)

---
+++ FunnyEcho/echo/webapp.py
@@ -0,0 +1,18 @@
+'''
+Created on 20/apr/2015
+
+@author: Fulvio Corno
+'''
+
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+Bootstrap(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
