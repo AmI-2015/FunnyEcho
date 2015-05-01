33f867920e10e2b17c186717a67cc4e11d781f26
fcorno <fulvio.corno@polito.it>
2015-05-01 14:55:59 +0200

    Version after class on 2015-04-30 - uses jQuery instead of plain Javascript - uses Ajax to offload string processing to the server - defines a POST method to manage string processing - shows how to encode/decode Json, both in jQuery.Ajax, and in Python/Flask

--- FunnyEcho/echo/static/script.js
+++ FunnyEcho/echo/static/script.js
@@ -1,16 +1,63 @@
-/**
- * 
- */
+// Register event handlers when is page is ready (han been loaded, and DOM is complete)
+$(document).ready(function() {
 
-function changed() {
-	var inputtext = document.getElementById("inputtext") ;
-	// reference to the DOM node of the <input> tag
-	
-	var text = inputtext.value ;
-	
-	var outputtext = document.getElementById("outputtext") ;
-	
-	outputtext.value = text ;
-	
+	// this code is executed when the inline function is called
+	// i.e., when the 'ready' event if fired by the document object
 
-}
+	// register handler on the input text
+	$('#inputtext').keyup(function() {
+
+		// this code is run at key-up event in the input text cell
+
+		// Query the HTML page for reading user-entered information
+		var text = $('#inputtext').val();
+
+		// prepare a Javascript object holding the data that we
+		// need to send to the server (the request parameters)
+		var requestdata = {
+			'text' : text,
+			'reverse' : $('#reverseBox').prop("checked"),
+			'flip' : $('#flipBox').prop("checked")
+		};
+
+		// Launch the request to the server
+		// See: http://api.jquery.com/jQuery.ajax/
+		$.ajax({
+			// remote URL and method type (for transferring JSON, always use
+			// POST)
+			url : '/process',
+			type : 'POST',
+
+			// Provide correct Content-Type, so that Flask will know how to
+			// process it.
+			contentType : 'application/json',
+			// Encode data as JSON. Data will be stored in the Http POST Request
+			// Body.
+			data : JSON.stringify(requestdata),
+
+			// This is the type of data expected back from the server.
+			// With this information, jQuery will already decode the response
+			// for us
+			dataType : 'json',
+
+			// The function that will be called when the respose returns
+			// The parameter 'ret' holds the data (response body), interpreted
+			// according
+			// to the 'dataType' that we set (i.e., a JS object created from
+			// JSON)
+			success : function(ret) {
+				// this code is run after the response is received
+
+				// may use the 'ret' object to extract info from the response
+				$('#outputtext').val(ret['text']);
+			} // end of success function
+
+		}); // end of .ajax call
+
+		// nothing more to do, now... the request is on its way, and
+		// we must wait untilt the response arrives (and the 'success' function
+		// is called
+		
+	}); // end of keyup event handler function
+
+}); // end of document ready event handler function
--- FunnyEcho/echo/webapp.py
+++ FunnyEcho/echo/webapp.py
@@ -4,8 +4,10 @@ Created on 20/apr/2015
 @author: Fulvio Corno
 '''
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, Response, jsonify
 from flask_bootstrap import Bootstrap
+
+from upsidedown import transform
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -13,6 +15,35 @@ Bootstrap(app)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/process', methods=["POST"])
+def processText():
+    
+    # decode the Json text from the request Body, returns a Python object
+    data = request.get_json()
+    
+    #extract from the Pithon object the fields that we are interested in
+    text = data['text']
+    
+    # process the string
+    if data['reverse'] :
+        text = text[::-1] 
+        # full-lenght slice of the string, with increment -1
+        # see https://docs.python.org/2.7/reference/expressions.html#slicings
+        # examples at: https://docs.python.org/2.7/whatsnew/2.3.html?highlight=slicing#extended-slices 
+        
+    if data['flip'] :
+        text = transform(text) 
+
+    # build the response object
+    result = { 'text': text }
+    # return the object in json format
+    return jsonify(result)
+
+    # if you don't use jsonify, you must build Json manually
+    ## return '{"text":"%s"}' % text
+    # or, better, also specify the correct mime type
+    ## return Response('{"text":"%s"}' % text, mimetype='application/json')    
 
 if __name__ == '__main__':
     app.run(debug=True)
